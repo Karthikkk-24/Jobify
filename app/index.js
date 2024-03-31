5979992b8e2748e5cf61a0bbc44e6afd55b49244
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-03-31 23:45:13 +0530

    Stack Screen Issue, added additional components and assets

--- app/index.js
+++ app/index.js
@@ -1,10 +1,41 @@
-import { Text, View } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+import { useState } from 'react';
+import { SafeAreaView, ScrollView, Text, View } from 'react-native';
+
+// import { icons, images } from '../constants';
+// import { COLORS, SIZES } from '../constants/theme';
+
+// import { Nearbyjobs, Popularjob, ScreenHeadBtn, Welcome } from '../components';
+import { COLORS, SIZES, icons, images } from '../constants';
 
 const Home = () => {
     return (
-        <View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
             <Text>Home</Text>
-        </View>
+            {/* <Stack.Screen options={{ 
+                headerStyle: {
+                    backgroundColor: COLORS.lightWhite
+                },
+                headerShadowVisible: false,
+                headerLeft: () => (
+                    <ScreenHeadBtn iconUrl={icons.menu} dimension="100%"/>
+                )
+             }} /> */}
+             <Stack
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: COLORS.lightWhite
+                    },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeadBtn iconUrl={icons.menu} dimension="100%"/>
+                    ),
+                    headerRight: () => (
+                        <ScreenHeadBtn iconUrl={images.profile} dimension="100%"/>
+                    )
+                }}
+             />
+        </SafeAreaView>
     )
 }
 
