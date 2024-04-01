fde4bfaaf700ef74ee8635e29d5fa8986fb2b2d5
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-04-01 23:39:19 +0530

    Version issue fixed

--- app/index.js
+++ app/index.js
@@ -1,42 +1,58 @@
-import { Stack, useRouter } from 'expo-router';
-import { useState } from 'react';
-import { SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { Stack, useRouter } from "expo-router";
+import { useState } from "react";
+import { SafeAreaView, ScrollView, View } from "react-native";
 
-// import { icons, images } from '../constants';
-// import { COLORS, SIZES } from '../constants/theme';
-
-// import { Nearbyjobs, Popularjob, ScreenHeadBtn, Welcome } from '../components';
-import { COLORS, SIZES, icons, images } from '../constants';
+import {
+    Nearbyjobs,
+    Popularjobs,
+    ScreenHeaderBtn,
+    Welcome,
+} from "../components";
+import { COLORS, icons, images, SIZES } from "../constants";
 
 const Home = () => {
-    return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-            <Text>Home</Text>
-            {/* <Stack.Screen options={{ 
-                headerStyle: {
-                    backgroundColor: COLORS.lightWhite
-                },
-                headerShadowVisible: false,
-                headerLeft: () => (
-                    <ScreenHeadBtn iconUrl={icons.menu} dimension="100%"/>
-                )
-             }} /> */}
-             <Stack
-                screenOptions={{
-                    headerStyle: {
-                        backgroundColor: COLORS.lightWhite
-                    },
-                    headerShadowVisible: false,
-                    headerLeft: () => (
-                        <ScreenHeadBtn iconUrl={icons.menu} dimension="100%"/>
-                    ),
-                    headerRight: () => (
-                        <ScreenHeadBtn iconUrl={images.profile} dimension="100%"/>
-                    )
-                }}
-             />
-        </SafeAreaView>
-    )
-}
+  const router = useRouter()
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
+          ),
+          headerTitle: "",
+        }}
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              if (searchTerm) {
+                router.push(`/search/${searchTerm}`)
+              }
+            }}
+          />
+
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default Home;
